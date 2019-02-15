b40bf3ea5d3205d410868917eb42cb281b5ac2d3
MichealPan9999 <panziqiang99@gmail.com>
2019-02-15 16:07:39 +0800

    menu菜单功能

--- app/src/main/java/com/example/panzq/a2019_1/MainActivity.java
+++ app/src/main/java/com/example/panzq/a2019_1/MainActivity.java
@@ -1,12 +1,16 @@
 package com.example.panzq.a2019_1;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Typeface;
 import android.support.annotation.Nullable;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -51,5 +55,38 @@ public class MainActivity extends AppCompatActivity {
         String strdm = " 手机屏幕信息：" + dm.toString();
         Log.d("panzqww", strMetrics);
         Log.d("panzqww", strdm);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        menu.add(0, 0, 0, R.string.about);
+        menu.add(0, 1, 1, R.string.exit);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case 0:
+                openOptionDialog();
+                break;
+            case 1:
+                finish();
+                break;
+        }
+        return true;
+    }
+
+    private void openOptionDialog() {
+        new AlertDialog.Builder(this)
+                .setTitle(R.string.about)
+                .setMessage("版本号 V1.1.8")
+                .setPositiveButton(R.string.confirm
+                        , new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+
+                            }
+                        }).show();
     }
 }
